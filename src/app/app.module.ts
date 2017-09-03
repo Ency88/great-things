c19e23d2818c6ad3d0af0ad766645997b563ebd6
Ency <e.kudra@gmail.com>
2017-09-03 12:21:38 +0200

    Basic layout of navigation and footer was implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import {AngularFireModule} from 'angularfire2';
 import {firebaseConfig} from '../environments/firebase.config';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
